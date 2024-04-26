4b02cf882a5e32ffb6d057930f017e43ddb74578
Marco <mavic030603@gmail.com>
2024-04-26 10:24:35 -0600

    - Agregar padecimientos y sus sintomas - Agregar niveles

--- database.pl
+++ database.pl
@@ -1,4 +1,7 @@
-/** Deportes */
+/** 
+Deportes 
+*/
+
 deporte(atletismo).
 deporte(ciclismo).
 deporte(natacion).
@@ -9,6 +12,8 @@ deporte(pesas).
 deporte(salto de cuerda).
 deporte(pesas).
 deporte(escalada).
+deporte(caminata).
+deporte(alpinismo).
 deporte(gimnasia).
 deporte(patinaje).
 deporte(patinaje en hielo).
@@ -21,26 +26,35 @@ deporte(badminton).
 deporte(tenis).
 deporte(alterofilia).
 
-/** Padecimientos */
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
-padecimiento().
+/** 
+Padecimientos
+(padecimiento, sintoma 1, sintoma 2, sintoma 3, sintoma 4, sintoma 5)
+ */
 
-/** Niveles */
-nivel().
-nivel().
-nivel().
-nivel().
-nivel().
+padecimiento(diabetes, debilidad, sed, hambre, entumcimiento, problemas de vision).
+padecimiento(cardiopatia, fatiga, debilidad, arritmia, falta de aliento, hinchazon).
+padecimiento(hernia, inflamacion, hinchazon, dolor, ardor, dificultad de movimiento).
+padecimiento(tendinitis, dolor, hinchazon, rigidez, sensibilidad, dolor de articulaciones).
+padecimiento(artritis, dolor, hinchazon, rigidez, fatiga, perdida motora).
+padecimiento(obesidad, fatiga, cansancio, dolor de articulaciones, ronquido, problemas de movimiento).
+padecimiento(sobrepeso, fatiga, cansancio, dolor de articulaciones, ronquido, sudor).
+padecimiento(depresion, fatiga, cansancio, tristeza, falta de motivacion, falta de apetito).
+padecimiento(asma, cansancio, fatiga, falta de aliento, dificultad al respirar, ronquido).
+padecimiento(cancer, muerte).
 
-/** Rutinas */
+/** 
+Niveles 
+(nivel, frecuencia semanal, tiempo, calorias)
+*/
+nivel(principiante, 2 dia, 1 hora, 200).
+nivel(incial, 2 dias, 1 hora, 300).
+nivel(intermedio, 3 dias, 2 horas, 800).
+nivel(avanzado, 4 dias, 2 horas, 1000).
+nivel(competitivo, 6 dias, 3 horas, 2000).
+
+/** 
+Rutinas 
+*/
 rutina().
 rutina().
 rutina().
