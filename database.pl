86b18ba378a77f93354b951f8e7e5a31bed98698
Marco <mavic030603@gmail.com>
2024-04-26 09:39:12 -0600

    agregar hehcos vacios a la "base de datos"

--- database.pl
+++ database.pl
@@ -1,7 +1,43 @@
 /** Deportes */
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
+deporte().
 
 /** Padecimientos */
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
+padecimiento().
 
 /** Niveles */
+nivel().
+nivel().
+nivel().
+nivel().
+nivel().
 
 /** Rutinas */
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
+rutina().
