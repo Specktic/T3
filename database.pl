3ee70445d7e4f4393d6242a9bf05c47117c066f1
Marco <mavic030603@gmail.com>
2024-04-30 23:03:45 -0600

    push commit

--- database.pl
+++ database.pl
@@ -2,53 +2,53 @@
 Deportes 
 */
 
-deporte(atletismo).
-deporte(ciclismo).
-deporte(natacion).
-deporte(danza).
-deporte(yoga).
-deporte(eliptica).
-deporte(pesas).
-deporte(salto de cuerda).
-deporte(pesas).
-deporte(escalada).
-deporte(caminata).
-deporte(alpinismo).
-deporte(gimnasia).
-deporte(patinaje).
-deporte(patinaje en hielo).
-deporte(futbol).
-deporte(futbol americano).
-deporte(basketball).
-deporte(baseball).
-deporte(rugby).
-deporte(badminton).
-deporte(tenis).
-deporte(alterofilia).
+deporte('atletismo').
+deporte('ciclismo').
+deporte('natacion').
+deporte('danza').
+deporte('yoga').
+deporte('eliptica').
+deporte('pesas').
+deporte('salto de cuerda').
+deporte('pesas').
+deporte('escalada').
+deporte('caminata').
+deporte('alpinismo').
+deporte('gimnasia').
+deporte('patinaje').
+deporte('patinaje en hielo').
+deporte('futbol').
+deporte('futbol americano').
+deporte('basketball').
+deporte('baseball').
+deporte('rugby').
+deporte('badminton').
+deporte('tenis').
+deporte('alterofilia').
 
 /** 
 Padecimientos
  */
 
-padecimiento(diabetes)
-padecimiento(cardiopatia)
-padecimiento(hernia)
-padecimiento(tendinitis)
-padecimiento(artritis)
-padecimiento(obesidad)
-padecimiento(sobrepeso)
-padecimiento(depresion)
-padecimiento(asma)
+padecimiento('diabetes')
+padecimiento('cardiopatia')
+padecimiento('hernia')
+padecimiento('tendinitis')
+padecimiento('artritis')
+padecimiento('obesidad')
+padecimiento('sobrepeso')
+padecimiento('depresion')
+padecimiento('asma')
 
 /** 
 Niveles 
 (nivel, frecuencia semanal, tiempo, calorias)
 */
-nivel(principiante, 2 dia, 1 hora, 200).
-nivel(incial, 2 dias, 1 hora, 300).
-nivel(intermedio, 3 dias, 2 horas, 800).
-nivel(avanzado, 4 dias, 2 horas, 1000).
-nivel(competitivo, 6 dias, 3 horas, 2000).
+nivel('principiante', 2, 1, 200).
+nivel('incial', 2, 1, 300).
+nivel('intermedio', 3, 2, 800).
+nivel('avanzado', 4, 2, 1000).
+nivel('competitivo', 6, 3, 2000).
 
 /** 
 Rutinas 
@@ -63,3 +63,4 @@ rutina().
 rutina().
 rutina().
 rutina().
+
