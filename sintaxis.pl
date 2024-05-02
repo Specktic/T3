028a79222986e5d38e5717a6bb2e154c24a3c739
Marco <mavic030603@gmail.com>
2024-05-01 22:33:00 -0600

    agregar verbos a sintaxis

--- sintaxis.pl
+++ sintaxis.pl
@@ -22,3 +22,12 @@ sustantivo-->[deporte];[].
 
 %Verbos
 verbo-->[].
+verbo-->[entrenar];[entreno];[entrenamos];[entrenaremos].
+verbo-->[ejercitar];[ejercito];[ejercitamos];[ejercitaremos].
+verbo-->[practicar];[practico];[practicamos];[practicaremos].
+verbo-->[correr];[corro];[corrimos];[correremos].
+verbo-->[andar];[ando];[anduvimos];[andare].
+verbo-->[manejar];[manejo];[manejamos];[manejaremos].
+verbo-->[nadar];[nado];[nadamos];[nadaremos].
+verbo-->[escalar];[escalo];[escalamos];[escalaremos].
+verbo-->[jugar];[juego];[jugamos];[jugaremos].
